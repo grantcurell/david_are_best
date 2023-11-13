189de3d0ca421293b1e1a1d21acdf5ea9ef1f53e
Grant Curell <grant_curell@dell.com>
2023-11-13 14:21:26 -0500

    Name changes

--- generate_plot.py
+++ generate_plot.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Load the data from a CSV file
 data = pd.read_csv('h2o_ising_model_predictions.csv')
@@ -11,7 +11,8 @@ grouped_data = data.groupby('temperature').agg(lambda x: abs(x).mean()).reset_in
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 
 # Plot the average of the absolute actual magnetization on the first subplot
-ax1.plot(grouped_data['temperature'], grouped_data['magnetization (actual)'], label='Average of Absolute Actual Magnetization', color='blue')
+ax1.plot(grouped_data['temperature'], grouped_data['magnetization (actual)'],
+         label='Average of Absolute Actual Magnetization', color='blue')
 ax1.set_xlabel('Temperature (T)')
 ax1.set_ylabel('Average of Absolute Actual Magnetization')
 ax1.set_title('Average of Absolute Actual Magnetization by Temperature')
@@ -19,7 +20,8 @@ ax1.legend()
 ax1.grid(True)
 
 # Plot the average of the absolute estimated magnetization on the second subplot
-ax2.plot(grouped_data['temperature'], grouped_data['magnetization'], label='Average of Absolute Estimated Magnetization', color='orange')
+ax2.plot(grouped_data['temperature'], grouped_data['magnetization'],
+         label='Average of Absolute Estimated Magnetization', color='orange')
 ax2.set_xlabel('Temperature (T)')
 ax2.set_ylabel('Average of Absolute Estimated Magnetization')
 ax2.set_title('Average of Absolute Estimated Magnetization by Temperature')
